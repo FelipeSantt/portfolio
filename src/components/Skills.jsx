3742a58c04267235a2e4f856ff2e94b4926ca8b2
Felipe Santos <felipek2267@gmail.com>
2024-09-17 00:52:24 -0300

    Implementando seções

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,7 @@ import { SKILLS } from "../constants";
 const Skills = () => {
   return (
     <div className="container mx-auto" id="skills">
-      <h2 className="mb-12 mt-20 text-center text-4xl font-semibold">Habilidades</h2>
+      <h2 className="mb-10 mt-20 text-center text-4xl font-semibold">Habilidades</h2>
       <div className="mx-2 flex flex-col rounded-3xl px-4 py-2 lg:px-20 border border-stone-50/30">
         {SKILLS.map((skill, index) => (
             <div key={index} className={`py-6 flex flex-items justify-between ${index != SKILLS.length - 1 ? "border-b border-stone-50/30 " : ""}`}>
